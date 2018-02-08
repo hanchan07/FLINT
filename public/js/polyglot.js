81312753dab029b6fc36edd4169f808fa233b444
hanchan07 <hanchan07@gmail.com>
2018-02-07 22:49:48 -0700

    Add l10ns and polyglot

---
+++ public/js/polyglot.js
@@ -0,0 +1,13 @@
+let polyglot = require('node-polyglot');
+
+function polyglotInit() {
+	//polyglot = new polyglot();
+}
+
+function doTheStuff() {
+	/*polyglot.extend({
+		hello: 'Hello Dude!',
+	});*/
+
+	console.log('hi there !!!!!!!!!!!!!');
+}
