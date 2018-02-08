9da843b8a23c31e2eca61846a019801fe648a370
Matt Young <mashu.daishi@gmail.com>
2018-02-08 12:42:27 -0700

    feature: add pluralization

--- public/js/i18next.js
+++ public/js/i18next.js
@@ -13,6 +13,12 @@ function i18nextInit() {
 						greeting:
 							'My name is {{user.firstName.roman}} {{user.lastName.roman}}.',
 						date: "Today's date is {{date, MM/DD/YYYY}}.",
+						plural: {
+						  "thing":                 "item",
+						  "thing_plural":          "items",
+						  "thingWithCount":        "{{count}} item",
+						  "thingWithCount_plural": "{{count}} items"
+						}
 					},
 				},
 				es: {
@@ -27,7 +33,7 @@ function i18nextInit() {
 						helloWorld: 'ハローワールド',
 						greeting:
 							'私の名前は{{user.lastName.kana}} {{user.firstName.kana}}です。',
-						date: '今日は{{date, 年YYYY月MM日DD}}です。',
+						date: '今日は{{date, 年YYYY月MM日DD}}です。'					
 					},
 				},
 				fr: {
@@ -62,6 +68,13 @@ function i18nextInit() {
 					return value;
 				},
 			},
+			plural: {
+				format: function(value, format, lng) {
+					if (value instanceof Date)
+						return moment(value).format(format);
+					return value;
+				},
+			},
 		},
 		function(err, t) {
 			// init set content
@@ -89,6 +102,16 @@ function updateContent() {
 	document.getElementById('interpolation').innerHTML = i18next.t('greeting', {
 		user,
 	});
+	document.getElementById('plural').innerHTML = [
+		i18next.t('plural.thing', {count: 0}),
+		i18next.t('plural.thing', {count: 1}),
+		i18next.t('plural.thing', {count: 5}),
+		i18next.t('plural.thing', {count: 100}),
+		i18next.t('plural.thingWithCount', {count: 0}),
+		i18next.t('plural.thingWithCount', {count: 1}),
+		i18next.t('plural.thingWithCount', {count: 5}),
+		i18next.t('plural.thingWithCount', {count: 100})
+	].join( '<br>' );
 	//Put this back in when we figure out how to implement moment.js on the front-end
 	document.getElementById('formatting').innerHTML = i18next.t('date', {
 		date: new Date(),
